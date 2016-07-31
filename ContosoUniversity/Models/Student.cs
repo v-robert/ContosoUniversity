fa511de13b754622a3d990ce62998e72b5c618b1
v-robert <vulic.robert@gmail.com>
2016-07-31 12:03:21 +0200

    v1
    31.07., 12:00

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        //s ovime imam problema koje se veže s kojim//
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
