61dd9d610603c06239bde6a33effd322ff6820e5
v-robert <vulic.robert@gmail.com>
2016-07-31 13:52:52 +0200

    v3
    error

--- ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -2,16 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
-namespace ContosoUniversity.DAL
-{
-    public class SchoolInitializer
-    {
-    }
-}using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data.Entity;
 using ContosoUniversity.Models;
 
